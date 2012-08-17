2d2663d24ff3c34049be9e41682de4464f272fbd
Maneesh Goel <maneeshgoel@gmail.com>
2012-08-17 16:26:50 -0700

    Initial Commit

---
+++ polling/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  polling
+//
+//  Created by Maneesh Goel on 8/17/12.
+//  Copyright (c) 2012 Maneesh Goel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ polling/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  polling
+//
+//  Created by Maneesh Goel on 8/17/12.
+//  Copyright (c) 2012 Maneesh Goel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
